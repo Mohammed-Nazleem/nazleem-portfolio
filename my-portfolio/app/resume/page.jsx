f475e6aceaf4e21a770d7694c4e7e4e5b42f38d3
Mohammed-Nazleem <nazleem18@gmail.com>
2024-06-18 09:45:52 +0530

    Updated recognitions data

--- my-portfolio/app/resume/page.jsx
+++ my-portfolio/app/resume/page.jsx
@@ -144,10 +144,21 @@ const skills = {
 
 // recognition data
 const recognition = {
-  icon: "",
-  title: "",
-  description: "",
-  
+  title: "My recognitions",
+  description: 
+  "Lorem ipsum dolor sit amet consectetur adipisicing elit. Illum, debitis. Quas, nemo temporibus vitae dignissimos suscipit.",
+  awardSet: [
+    {
+      name: "Best academic student of the year",
+      year: 2014,
+      college: "British College",
+    },
+    {
+      name: "Most outstanding student of the year",
+      year: 2015,
+      college: "Edexcel",
+    },
+  ],
 };
 
 const Resume = () => {
@@ -173,8 +184,8 @@ const Resume = () => {
             <TabsTrigger value="experience">Experience</TabsTrigger>
             <TabsTrigger value="education">Education</TabsTrigger>
             <TabsTrigger value="skills">Skills</TabsTrigger>
-            <TabsTrigger value="about">About me</TabsTrigger>
             <TabsTrigger value="recognition">Recognitions</TabsTrigger>
+            <TabsTrigger value="about">About me</TabsTrigger>
           </TabsList>
 
           {/* content */}
@@ -270,6 +281,37 @@ const Resume = () => {
                     );
                   })}
                 </ul>
+              </div>
+            </TabsContent>
+
+            {/* recognitions */}
+            <TabsContent value="recognition" className="w-full">
+              <div className="flex flex-col gap-[30px] text-center xl:text-left">
+                <h3 className="text-4xl font-bold capitalize">{recognition.title}</h3>
+                <p className="max-w-[600px] text-white/60 mx-auto xl:mx-0">
+                  {recognition.description}
+                </p>
+                <ScrollArea className="h-[400px]">
+                  <ul className="grid grid-cols-1 lg:grid-cols-2 gap-[30px]">
+                    {recognition.awardSet.map((item, index) => {
+                      return (
+                        <li
+                          key={index}
+                          className="bg-[#232329] h-[184px] py-6 px-10 rounded-xl flex flex-col justify-center items-center lg:items-start gap-1"
+                        >
+                          <span className="text-accent">{item.year}</span>
+                          <h3 className="text-xl max-w-[260px] min-h-[80px] text-center lg:text-left">
+                            {item.name}
+                          </h3>
+                          <div className="flex items-center gap-3">
+                            <span className="w-[6px] h-[6px] rounded-full bg-accent"></span>
+                            <p className="text-white/60">{item.college}</p>
+                          </div>
+                        </li>
+                      );
+                    })}
+                  </ul>
+                </ScrollArea>
               </div>
             </TabsContent>
 
